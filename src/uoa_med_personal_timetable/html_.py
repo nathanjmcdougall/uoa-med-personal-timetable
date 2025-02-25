fb55f68dc9cf5664f498aedda9a66827f6527931
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2025-02-25 17:18:19 +1300

    Change contact info

--- src/uoa_med_personal_timetable/html_.py
+++ src/uoa_med_personal_timetable/html_.py
@@ -6,13 +6,8 @@ def footer():
     return """\
 <br clear=all>
 <hr>
-<form method=post action=f.php>
-   <small>
-      <h2>Feedback</h2>
-      <a name=feedback>We love criticism. It doesn't have to be constructive. You can <a href="mailto:waynemcdougall+med@gmail.com">email us</a> if you have any comments, questions or complaints</a> or you can fill out this anonymous form:
-      <br>
-      <textarea name=message style='width:99%;height:5vh' placeholder="Type your feedback here..."></textarea>
-      <br>Optional email or phone number if you want a reply: <input type=text name=contact><br><input type=submit value="Send Feedback">
-   </small>
-</form>
+<small>
+    <h2>Feedback</h2>
+    <a name=feedback>You can <a href="mailto:nathanjmcdougall@gmail.com">email us</a> if you have any comments, questions or complaints.</a>
+</small>
 """
